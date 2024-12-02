d69368798e0506bfe45f54380d514ca1c03a3834
Papun1111 <gohanmohapatra@gmail.com>
2024-12-02 22:51:35 +0530

    updated ques 12

--- ques12.py
+++ ques12.py
@@ -1,15 +1,15 @@
-l=[[1,2,3],[4,5,6],[7,8,9]]
-
-def sumofr(l,r,c):
-    sum=0
-    for i in range(0,r+1):
-        for j in range(0,c):
-            sum+=l[i][j]
-    return sum
-
-for i in range(0,3):
+l=[[1,2,3,4],[4,5,6,5],[7,8,9,6]]
+sum=[]
+sumc=0
+def sumcol(l,i):
     sum=0
     for j in range(0,3):
-        sum+=sumofr(l,i,3)
-        print(f"{l[i][j]}",end=" ")
-    print(sum)
+        sum+=l[j][i]
+    return sum
+for i in range(0,4):
+    sumc=sumcol(l,i)
+    sum.append(sumc)
+    sumc=0
+print(sum)
+
+
