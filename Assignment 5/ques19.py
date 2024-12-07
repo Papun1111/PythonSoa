130a3fd0d4193c20aab6939148f77fafb947e484
Papun1111 <gohanmohapatra@gmail.com.com>
2024-12-07 23:12:43 +0530

    assignment completed

--- Assignment 5/ques19.py
+++ Assignment 5/ques19.py
@@ -1,1 +1,14 @@
-words=["a","quick","brown","fox","jumps","across","a","black","lazy","dog","cat"]
+
+def unique_pairs(words):
+    newlist=[x for x in words if len(x)>=4]
+    newlist.sort()
+    res=set()
+    for i in range(len(newlist)):
+        for j in range(i+1,len(newlist)):
+            word1=newlist[i]
+            word2=newlist[j]
+            if set(word1).isdisjoint(set(word2)):
+                res.add((word1,word2))
+    return res
+words = ["apple", "dogs", "cat", "bird", "fish", "zebra", "lion"]
+print(unique_pairs(words))
